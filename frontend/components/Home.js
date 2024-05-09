d9530539f966259c48ae1db21431574fa3b0eafa
KeithLamb <keithianlamb@gmail.com>
2024-05-09 11:01:48 -0500

    set up routing and finsih checking and setting up valid postman return for tests and API

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import pizza from './images/pizza.jpg'
 
 function Home() {
+  const navigate = useNavigate()
+  const goToOrder = () => navigate('/order')
   return (
     <div>
       <h2>
         Welcome to Bloom Pizza!
       </h2>
-      {/* clicking on the img should navigate to "/order" */}
-      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza} />
+      <img alt="order-pizza" style={{ cursor: 'pointer' }} src={pizza}  onClick={goToOrder}/>
     </div>
   )
 }
